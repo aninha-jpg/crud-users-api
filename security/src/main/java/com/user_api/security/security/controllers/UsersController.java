33f5f63b54fff7e2413f1194fb081a787f09b26c
Ana <gtana.dev@gmail.com>
2026-09-20 19:41:15 -0300

    feat: new endpoint for users

--- security/src/main/java/com/user_api/security/security/controllers/UsersController.java
+++ security/src/main/java/com/user_api/security/security/controllers/UsersController.java
@@ -5,6 +5,7 @@ import com.user_api.security.security.repositories.UsersRepository;
 import com.user_api.security.security.DTO.UsersRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import com.user_api.security.security.DTO.UsersResponseDTO;
@@ -35,6 +36,15 @@ public class UsersController {
         Users usersData = new Users(data);
         usersData.setSenha(hashPassword);
         repository.save(usersData);
+    }
+    //lista as informações do usuario
+    @GetMapping("/me")
+    public ResponseEntity<UsersResponseDTO> getMe(Authentication authentication) {
+        String email = authentication.getName();
+
+        Users user = repository.findByEmail(email);
+
+        return ResponseEntity.ok(new UsersResponseDTO(user));
     }
 
 }
